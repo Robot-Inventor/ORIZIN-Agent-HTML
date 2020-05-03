7027abc215a4606b5070694d19f8f8feb503c530
Robot-Inventor <30.keita@gmail.com>
2020-05-03 16:09:32 +0900

    タイマーアプリの設定時間をあとから変更できる機能を追加

--- timer.py
+++ timer.py
@@ -6,19 +6,24 @@ import sys
 
 
 arg = sys.argv
-HOUR = arg[1]
-MINUTE = arg[2]
-SECOND = arg[3]
+if len(arg) >= 3:
+    HOUR = arg[1]
+    MINUTE = arg[2]
+    SECOND = arg[3]
+else:
+    HOUR = 0
+    MINUTE = 0
+    SECOND = 0
 
 
 @eel.expose
 def timer(hour, minute, second):
     eel.update_time(f"{hour}:{minute}:{second}")
-    if type(hour) == str:
+    if type(hour) != int:
         hour = int(hour)
-    if type(minute) == str:
+    if type(minute) != int:
         minute = int(minute)
-    if type(second) == str:
+    if type(second) != int:
         second = int(second)
     while hour >= 0 and minute >= 0 and second >= 0:
         eel.sleep(1)
