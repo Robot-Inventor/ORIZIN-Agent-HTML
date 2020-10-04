41156b20aba3420c44a73eb18ef299ebccde6ba1
Robot-Inventor <30.keita@gmail.com>
2020-10-04 17:38:40 +0900

    一部のUIにORIZIN UIを導入

--- resource/javascript/basic.js
+++ resource/javascript/basic.js
@@ -9,30 +9,6 @@ async function readable_text_setting() {
     }
 }
 
-function toggle_switch_component() {
-    toggle_element_num = 0;
-    document.querySelectorAll("input[type=checkbox].toggle").forEach(function(toggle_element) {
-        toggle_id = toggle_element.getAttribute("id");
-        if (!toggle_id) {
-            toggle_element.setAttribute("id", "toggle_element_" + toggle_element_num);
-            toggle_id = "toggle_element_" + toggle_element_num;
-            toggle_element_num += 1;
-        }
-        toggle_element.insertAdjacentHTML("afterend", "<label for='" + toggle_id + "' class='ripple_effect'></label>");
-    });
-}
-
-function underline_textbox_cmponent() {
-    input_element_num = 0;
-    document.querySelectorAll("input[type=text].underline_textbox").forEach(function(input_element) {
-        input_group_id = "input_group_" + input_element_num;
-        input_element.insertAdjacentHTML("afterend", "<div class='text_input_group' id='" + input_group_id + "'><div class='text_underline'></div></div>");
-        text_input_group = document.querySelector("div.text_input_group#" + input_group_id);
-        text_input_group.insertBefore(input_element, document.querySelector("div.text_input_group#" + input_group_id + " div.text_underline"));
-        input_element_num += 1;
-    });
-}
-
 $(function() {
     $("header").load("basic.html");
     eel.print_log_if_dev_mode("Page header loaded.", {"Status": "OK"});
@@ -49,7 +25,5 @@ $(function() {
         easing: 'linear'
     });
     readable_text_setting();
-    toggle_switch_component();
-    underline_textbox_cmponent();
     eel.print_log_if_dev_mode("Page rendered.", {"File": location.pathname});
 });
