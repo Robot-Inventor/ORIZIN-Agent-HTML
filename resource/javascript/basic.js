e79bf9b0b9a4c701e1cb67050841ed1254bdf041
Robot-Inventor <30.keita@gmail.com>
2021-01-06 23:40:55 +0900

    ページ読み込み時のレイアウトシフトを軽減

--- resource/javascript/basic.js
+++ resource/javascript/basic.js
@@ -9,24 +9,6 @@ async function readable_text_setting() {
     }
 }
 
-async function load_header() {
-    const response = await fetch("basic.html");
-    const data = await response.text();
-    document.querySelector("header").innerHTML = data;
-    new Ripple(".ripple_effect", {
-        debug: false,
-        on: "mousedown",
-        opacity: 0.4,
-        color: "auto",
-        multi: false,
-        duration: 0.7,
-        rate: function(pxPerSecond) {
-            return pxPerSecond;
-        },
-        easing: "linear"
-    });
-}
-
 async function load_file(file_path, target_element) {
     const fetch_response = await fetch(file_path);
     const response_content = await fetch_response.text();
@@ -76,10 +58,69 @@ async function move_page(place) {
     }, 300);
 }
 
-window.addEventListener("load", () => {
-    load_header();
-    eel.print_log_if_dev_mode("Page header loaded.", {"Status": "OK"});
-    readable_text_setting();
-    feed_buck_setting();
-    eel.print_log_if_dev_mode("Page rendered.", {"File": location.pathname});
+
+document.querySelector("header").innerHTML = `
+<div id="title">
+    <span class="ripple_effect"><img src="../image/menu_icon.svg" id="menu_open_button" onclick="open_menu()" title="メニューを開く"></span><span class="ripple_effect"><a href="index.html" title="ホーム画面へ戻る">ORIZIN Agent HTML</a></span>
+</div>
+<div id="side_menu_bar_overlay" onclick="close_menu()"></div>
+<div id="side_menu_bar">
+    <button id="menu_close_button" class="ripple_effect" onclick="close_menu()" title="メニューを閉じる"><i class="material_icon">arrow_forward</i></button>
+    <div class="ripple_effect" onclick="move_page('index.html')" title="ホーム画面に戻る">
+        <i class="material_icon">home</i>ホーム
+    </div>
+    <hr>
+    <div class="ripple_effect" onclick="move_page('setting.html')" title="様々な設定をする">
+        <i class="material_icon">settings</i>設定
+    </div>
+    <hr>
+    <div class="ripple_effect" onclick="move_page('oss_license.html')" title="使用しているOSSの名称とライセンスの一覧を見る">
+        <i class="material_icon">assignment</i>オープンソースソフトウェアライセンス
+    </div>
+    <hr>
+    <div class="ripple_effect" onclick="move_page('privacy_policy.html')" title="ユーザーの情報の扱い方について見る">
+        <i class="material_icon">privacy_tip</i>プライバシーポリシー
+    </div>
+    <hr>
+    <div class="ripple_effect" onclick="move_page('feedback.html')" id="feedback_button" title="フィードバックを送信する">
+        <i class="material_icon">feedback</i>フィードバック
+    </div>
+    <hr>
+    <div class="ripple_effect" onclick="move_page('about_oa.html')" title="バージョンやソフトウェアの概要を見る">
+        <i class="material_icon">info</i>ORIZIN Agentについて
+    </div>
+    <hr>
+    <a href="https://github.com/Robot-Inventor/ORIZIN-Agent-HTML/wiki" target="_blank" rel="noreferrer noopener">
+        <div title="使い方が書かれているサイトを開く" class="ripple_effect">
+            <i class="material_icon">help</i>ヘルプ＆Wiki(GitHub)<i class="material_icon">open_in_new</i>
+        </div>
+    </a>
+    <hr>
+    <div id="copyright_in_menu">
+        Copyright &copy; 2019 - 2021 Robot-Inventor
+    </div>
+</div>
+<style>
+    #feedback_button {
+        display: none;
+    }
+</style>
+`;
+
+new Ripple(".ripple_effect", {
+    debug: false,
+    on: "mousedown",
+    opacity: 0.4,
+    color: "auto",
+    multi: false,
+    duration: 0.7,
+    rate: function(pxPerSecond) {
+        return pxPerSecond;
+    },
+    easing: "linear"
 });
+
+eel.print_log_if_dev_mode("Page header loaded.", {"Status": "OK"});
+readable_text_setting();
+feed_buck_setting();
+eel.print_log_if_dev_mode("Page rendered.", {"File": location.pathname});
