df62fec87c7ca8df88366a426a7511d85e6fcc54
Robot-Inventor <30.keita@gmail.com>
2021-01-11 02:19:38 +0900

    UIの一部をMaterial Web Componentsに置き換え

--- resource/javascript/import.js
+++ resource/javascript/import.js
@@ -2,3 +2,6 @@ import "@material/mwc-button";
 import "@material/mwc-snackbar";
 import "@material/mwc-icon-button";
 import "@material/mwc-dialog";
+import "@material/mwc-switch";
+import "@material/mwc-radio";
+import "@material/mwc-formfield";
